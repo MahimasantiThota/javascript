e8f2438f985090a8c7034494109a80c9adb04c09
t.mahima <mahimathota3@gmail.com>
2026-06-03 11:20:18 +0530

    datatypes

--- intro/NumberDataType/numberDataType.js
+++ intro/NumberDataType/numberDataType.js
@@ -1,41 +1,13 @@
-function addNumbers(){
-    debugger;
-    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
-    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var sum = firstNumber + secondNumber;
-    alert(sum);
-    
-}
-function subNumbers(){
-    debugger;
-    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
-    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var sum = firstNumber + secondNumber;
-    alert(sum);
-    
-}
-function mulNumbers(){
-    debugger;
-    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
-    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var sum = firstNumber + secondNumber;
-    alert(sum);
-    
-}
-function divNumbers(){
-    debugger;
-    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
-    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var sum = firstNumber + secondNumber;
-    alert(sum);
-    
-}
-function remNumbers(){
-    debugger;
-    var firstNumber = Number(document.getElementById("txtFirstNumber").value);
-    var secondNumber = Number(document.getElementById("txtSecondNumber").value);
-    var sum = firstNumber + secondNumber;
-    alert(sum);
-    
-}
+function onClickAdd() {
 
+    let firstNumber =
+        Number(document.getElementById("txtFirstNumber").value);
+
+    let secondNumber =
+        Number(document.getElementById("txtSecondNumber").value);
+
+    let sum = firstNumber + secondNumber;
+
+    document.getElementById("result").innerHTML =
+        "Result = " + sum;
+}
