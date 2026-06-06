d9d29b6fd2a9c8a8baecb12f6f20946e0af853b1
t.mahima <mahimathota3@gmail.com>
2026-06-06 13:54:00 +0530

    add,sub,mul,div

--- intro/NumberDataType/numberDataType.js
+++ intro/NumberDataType/numberDataType.js
@@ -1,13 +1,30 @@
-function onClickAdd() {
-
-    let firstNumber =
-        Number(document.getElementById("txtFirstNumber").value);
-
-    let secondNumber =
-        Number(document.getElementById("txtSecondNumber").value);
-
-    let sum = firstNumber + secondNumber;
-
-    document.getElementById("result").innerHTML =
-        "Result = " + sum;
+function onClickAdd(){
+    debugger;
+    var firstnumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondnumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstnumber + secondnumber;
+    alert(sum);
 }
+function onClickSub(){
+    debugger;
+    var firstnumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondnumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstnumber - secondnumber;
+    alert(sum);
+}
+function onClickDiv(){
+    debugger;
+    var firstnumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondnumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstnumber / secondnumber;
+    alert(sum);
+}
+function onClickMul(){
+    debugger;
+    var firstnumber = Number(document.getElementById("txtFirstNumber").value);
+    var secondnumber = Number(document.getElementById("txtSecondNumber").value);
+    var sum = firstnumber * secondnumber;
+    alert(sum);
+}
+
+
