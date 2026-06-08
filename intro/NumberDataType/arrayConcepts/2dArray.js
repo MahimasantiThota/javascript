cf953424f531406a9868dfa4491ed84879ef5a67
t.mahima <mahimathota3@gmail.com>
2026-06-08 12:17:20 +0530

    arrays

---
+++ intro/NumberDataType/arrayConcepts/2dArray.js
@@ -0,0 +1,12 @@
+var nandGate = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 0, 0],
+    [1, 1, 1]
+];
+function onClick2dArray() {
+    debugger;
+    var inputIndex = Number(document.getElementById("txtInputIndex").value);
+    var secondIndex = Number(document.getElementById("txtSecondIndex").value);
+    document.getElementById("pResult").innerHTML = nandGate[inputIndex][secondIndex];
+}
