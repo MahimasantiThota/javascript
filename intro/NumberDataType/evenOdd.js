921ae0493c9ada6e15bcd931b8fd9c8220e863c1
t.mahima <mahimathota3@gmail.com>
2026-06-08 11:18:20 +0530

    evenconcepts

--- intro/NumberDataType/evenOdd.js
+++ intro/NumberDataType/evenOdd.js
@@ -1,6 +1,6 @@
 function onClickNumber() {
     debugger;
-    var number = Number(document.getElementById("inputNumber").value);
+    var number = Number(document.getElementById("txtinputNumber").value);
     var result = document.getElementById("pResult");
 
     if (number % 2 == 0) {
